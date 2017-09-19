cf195915aecc2823c9ba5c850547e66b09083c8a
alberto56 <albert@mediatribe.net>
2017-09-19 10:35:40 -0400

    c

--- developer/frameworks/drupal8/docker-resources/uninstall-comment-module.sh
+++ developer/frameworks/drupal8/docker-resources/uninstall-comment-module.sh
@@ -5,6 +5,8 @@
 
 set -e
 
+echo "About to delete all entites of type comment, so we can uninstall the comment module."
 drush eval "\Drupal::entityManager()->getStorage('field_config')->load('node.article.comment')->delete();"
+echo "There should not longer be any comments at this point."
 drush cron
 drush -y pm-uninstall comment
