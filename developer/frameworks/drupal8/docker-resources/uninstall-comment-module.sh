f0279c2f7ca2fbe63a15f5d08d218f7762d9e55f
Albert Albala <albert@mediatribe.net>
2017-10-23 14:15:26 -0400

    Correctly remove the comment module during the testing process (#14)

--- developer/frameworks/drupal8/docker-resources/uninstall-comment-module.sh
+++ developer/frameworks/drupal8/docker-resources/uninstall-comment-module.sh
@@ -5,6 +5,14 @@
 
 set -e
 
-drush eval "\Drupal::entityManager()->getStorage('field_config')->load('node.article.comment')->delete();"
+echo 'About to delete the comment module.'
+echo 'Deleting all comment entity content...'
+# We want to interpret $comment as a literal, not a variable.
+# shellcheck disable=SC2016
+drush ev 'foreach(entity_load_multiple("comment") as $comment) { $comment->delete(); }'
+echo 'Deleting the comment storage...'
+drush ev "\Drupal::entityManager()->getStorage('field_config')->load('node.article.comment')->delete();"
+echo 'Run cron to avoid the fields pending deletion issue...'
 drush cron
+echo 'Uninstalling the comme module...'
 drush -y pm-uninstall comment
