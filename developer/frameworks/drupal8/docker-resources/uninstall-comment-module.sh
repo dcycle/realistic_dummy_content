e89daf6569350abfb65603e942bc55ee37604023
alberto56 <albert@mediatribe.net>
2017-10-23 13:27:20 -0400

    c

--- developer/frameworks/drupal8/docker-resources/uninstall-comment-module.sh
+++ developer/frameworks/drupal8/docker-resources/uninstall-comment-module.sh
@@ -7,6 +7,8 @@ set -e
 
 echo 'About to delete the comment module.'
 echo 'Deleting all comment entity content...'
+# We want to interpret $comment as a literal, not a variable.
+# shellcheck disable=SC2016
 drush ev 'foreach(entity_load_multiple("comment") as $comment) { $comment->delete(); }'
 echo 'Running cron to delete the actual comments...'
 drush cron
