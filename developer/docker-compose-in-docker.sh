f3e0921a4377baab8ba66601ef30482a0d2f6a31
alberto56 <albert@mediatribe.net>
2017-11-03 08:41:36 -0400

    c

---
+++ developer/docker-compose-in-docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Use ./docker-compose-in-docker.sh ... instead of docker-compose ...
+# We do not want to make assumptions about whether or not docker-compose
+# is installed on a given platform (for example CoreOS does not have
+# docker-compose). In keeping with the philosophy of Docker of keeping
+# every dependency in a container, docker-compose being a dependency, we
+# use it within a container.
+#
+
+docker run \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v "$PWD:/rootfs/$PWD" \
+  -w="/rootfs/$PWD" \
+  docker/compose $@
