e1085c7116dfa1fb2c5cb8953a31ce33fe8b8507
alberto56 <albert@mediatribe.net>
2020-09-27 10:55:41 -0400

    Please squash this commit in the pull request

--- scripts/test-running-environment.sh
+++ scripts/test-running-environment.sh
@@ -16,13 +16,13 @@ echo " => Uninstalling comment module"
 docker-compose exec drupal /bin/bash -c '/var/www/html/modules/custom/realistic_dummy_content/scripts/lib/docker-resources/uninstall-comment-module.sh'
 echo -e 'Make sure we can run generate-realistic even if the'
 echo -e 'comment module is disabled.'
-if [ -v $1 ]; then
+if [ -v "$1" ]; then
   docker-compose exec "$1" /bin/bash -c 'drush generate-realistic'
 fi
 echo -e 'Reenable comment and make sure we can run'
 echo -e 'generate-realistic.'
 docker-compose exec drupal /bin/bash -c 'drush en -y comment'
-if [ -v $1 ]; then
+if [ -v "$1" ]; then
   docker-compose exec "$1" /bin/bash -c 'drush generate-realistic'
 fi
 
