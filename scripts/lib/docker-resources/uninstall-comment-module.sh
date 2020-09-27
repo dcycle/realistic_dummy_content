07e8b2d6eb6c1cb61a29902bbd851965a81816b6
alberto56 <albert@mediatribe.net>
2020-09-27 11:07:09 -0400

    Please squash this commit in the pull request

--- scripts/lib/docker-resources/uninstall-comment-module.sh
+++ scripts/lib/docker-resources/uninstall-comment-module.sh
@@ -9,7 +9,7 @@ echo 'About to delete the comment module.'
 echo 'Deleting all comment entity content...'
 # We want to interpret $comment as a literal, not a variable.
 # shellcheck disable=SC2016
-drush ev 'foreach(entity_load_multiple("comment") as $comment) { $comment->delete(); }'
+drush ev 'foreach(\Drupal::entityManager()->getStorage("comment")->loadMultiple() as $comment) { $comment->delete(); }'
 echo 'Deleting the comment storage...'
 drush ev "\Drupal::entityManager()->getStorage('field_config')->load('node.article.comment')->delete();"
 echo 'Run cron to avoid the fields pending deletion issue...'
