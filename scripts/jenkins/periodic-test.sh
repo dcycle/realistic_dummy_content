79a459e5a7eacd254a3220b8b395daabec54eeda
alberto56 <albert@mediatribe.net>
2020-10-25 11:42:07 -0400

    Add Jenkins period test script

---
+++ scripts/jenkins/periodic-test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+source ~/.docker-host-ssh-credentials
+
+# Create a droplet
+PRIVATE_IP=$(ssh "$DOCKERHOSTUSER"@"$DOCKERHOST" \
+  "./digitalocean/scripts/new-droplet.sh realistic-dummy-content")
+# https://github.com/dcycle/docker-digitalocean-php#public-vs-private-ip-addresses
+IP=$(ssh "$DOCKERHOSTUSER"@"$DOCKERHOST" "./digitalocean/scripts/list-droplets.sh" |grep "$PRIVATE_IP" --after-context=10|tail -1|cut -b 44-)
+echo "Created Droplet at $IP"
+sleep 90
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+  root@"$IP" \
+  "git clone http://github.com/dcycle/realistic_dummy_content && \
+  ./scripts/ci.sh"
