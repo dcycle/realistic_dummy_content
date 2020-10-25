724573359dd70329999a02e2bfb8a45e56982e63
alberto56 <albert@mediatribe.net>
2020-10-25 19:56:58 -0400

    Please squash this commit in the pull request

--- scripts/jenkins/periodic-test.sh
+++ scripts/jenkins/periodic-test.sh
@@ -3,10 +3,10 @@ set -e
 source ~/.docker-host-ssh-credentials
 
 # Create a droplet
-PRIVATE_IP=$(ssh "$DOCKERHOSTUSER"@"$DOCKERHOST" \
+PRIVATE_IP=$(ssh "$DOCKERHOSTUSER@$DOCKERHOST" \
   "./digitalocean/scripts/new-droplet.sh realistic-dummy-content")
 # https://github.com/dcycle/docker-digitalocean-php#public-vs-private-ip-addresses
-IP=$(ssh "$DOCKERHOSTUSER"@"$DOCKERHOST" "./digitalocean/scripts/list-droplets.sh" |grep "$PRIVATE_IP" --after-context=10|tail -1|cut -b 44-)
+IP=$(ssh "$DOCKERHOSTUSER@$DOCKERHOST" "./digitalocean/scripts/list-droplets.sh" |grep "$PRIVATE_IP" --after-context=10|tail -1|cut -b 44-)
 echo "Created Droplet at $IP"
 sleep 90
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
