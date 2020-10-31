964fc959ec9c2f7f069a842c2cd85facd2a99ceb
alberto56 <albert@mediatribe.net>
2020-10-30 21:31:04 -0400

    Please squash this commit in the pull request

--- scripts/php-drupal9.sh
+++ scripts/php-drupal9.sh
@@ -5,6 +5,4 @@
 set -e
 
 echo "=> Fail if non-Drupal 9 code is present in codebase"
-# See https://github.com/dcycle/docker-drupal-check on why we're using
-# this weird tag
-docker run --rm -v "$(pwd)":/var/www/html/modules/realistic_dummy_content dcycle/drupal-check:1.2019-12-30-21-59-43-UTC realistic_dummy_content/api/src
+docker run --rm -v "$(pwd)":/var/www/html/modules/realistic_dummy_content dcycle/drupal-check:1 realistic_dummy_content/api/src
